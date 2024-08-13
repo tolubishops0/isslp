803da668bcc23a50236212f15db73ac9028d0c85
Tolu Okunjoyo <toluokunjoyo@gmail.com>
2024-08-13 18:49:02 +0100

    ai chat screen updates

--- src/components/LandingPage/Potential.tsx
+++ src/components/LandingPage/Potential.tsx
@@ -45,9 +45,9 @@ export default function Potential() {
             Unlock Your Potential with Intelligent Scholar
           </h1>
           <p className="text-[#FFFDFD] font-light text-base md:text-lg text-center mt-4">
-            From research to exams, weve got you covered. Our comprehensive
-            services are designed to assist students at every stage of their
-            academic journey.
+            From research to exams, we&apos;ve got you covered. Our
+            comprehensive services are designed to assist students at every
+            stage of their academic journey.
           </p>
         </div>
 
--- src/components/LandingPage/Solutions.tsx
+++ src/components/LandingPage/Solutions.tsx
@@ -13,9 +13,9 @@ export default function Solutions() {
             Get help with Intelligent Scholar AI
           </h1>
           <p className="text-[#FFFDFD] font-extralight text-sm text-center">
-            Dont struggle alone. Intelligent Scholar AI is here to assist you.
-            Experience the power of AI-driven learning. Try it now and see the
-            difference.
+            Don&apos;t struggle alone. Intelligent Scholar AI is here to assist
+            you. Experience the power of AI-driven learning. Try it now and see
+            the difference.
           </p>
 
           <Link href={"/chat-with-ai"}>
